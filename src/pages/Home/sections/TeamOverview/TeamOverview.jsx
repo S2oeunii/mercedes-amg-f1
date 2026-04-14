fcbbe1f9b02bcd9b754b41bf31e0f70f2643a1d2
소은 <soeunl131@gmail.com>
2026-04-14 21:25:55 +0900

    반응형 수정

--- src/pages/Home/sections/TeamOverview/TeamOverview.jsx
+++ src/pages/Home/sections/TeamOverview/TeamOverview.jsx
@@ -2,6 +2,7 @@ import { forwardRef, useEffect, useRef } from 'react';
 import precision from '../../../../assets/img/team/precision.png';
 import performance from '../../../../assets/img/team/performance.png';
 import legacy from '../../../../assets/img/team/legacy.png';
+import arrow from '../../../../assets/img/team/arrow.svg';
 import MoreView from '../../../../components/buttons/MoreView';
 
 const TeamOverview = forwardRef(({ onScrollUp, isActive }, ref) => {
@@ -29,22 +30,22 @@ const TeamOverview = forwardRef(({ onScrollUp, isActive }, ref) => {
       ref={ref}
     >
       <section className='overview w-full lg:w-[1360px] px-12 lg:px-0'>
-        <span className="text-[#00F4D0]/70 font-pretendard font-semibold text-[26px] leading-none tracking-none">Team Overview</span>
+        <span className="text-[#00F4D0]/70 font-pretendard font-semibold text-[14px] lg:text-[26px] leading-none tracking-none">Team Overview</span>
 
-        <div className='container w-full flex flex-col gap-[123px] justify-center items-center'>
-          <div className='contents w-full flex flex-col lg:flex-row justify-between mx-auto mt-25'>
+        <div className='container w-full flex flex-col gap-10 lg:gap-[123px] justify-center items-center'>
+          <div className='contents w-full flex flex-col lg:flex-row gap-10 lg:gap-0 justify-between mx-auto mt-25'>
             <div className='desc'>
-              <h1 className="w-[636px] h-auto mb-10
-                text-[#fff] font-archivo font-bold italic text-[80px] leading-none tracking-none"
+              <h1 className="w-[255px] lg:w-[636px] h-auto mb-10
+                text-[#fff] font-archivo font-bold italic text-[35px] lg:text-[80px] leading-none tracking-none"
               >
                 Precison.<br />
                 <p className='text-right'>Performance.</p>
                 Legacy.
               </h1>
 
-              <p className='text-[#C0C7CE] font-pretendard font-light text-[20px] leading-normal tracking-tight'>
+              <p className='text-[#C0C7CE] font-pretendard font-light text-[14px] lg:text-[20px] leading-normal tracking-tight'>
                 독일을 대표하는 메르세데스-AMG 페트로나스 포뮬러 원 팀은<br />
-                수십 년의 레이싱 헤리티지를 바탕으로 최점단 기술력과  정밀한 엔지니어링을 결합해,<br />
+                수십 년의 레이싱 헤리티지를 바탕으로<br className="lg:hidden" /> 최첨단 기술력과  정밀한 엔지니어링을 결합해,<br />
                 퍼포먼스의 한계를 끊임없이 확장해 나갑니다.
               </p>
             </div>
@@ -94,8 +95,20 @@ const TeamOverview = forwardRef(({ onScrollUp, isActive }, ref) => {
         </div>
       </section>
 
-      <section className='drivers'>
+      <section className='drivers w-full lg:w-[1360px] px-12 lg:px-0'>
+        <span className="text-[#00F4D0]/70 font-pretendard font-semibold text-[14px] lg:text-[26px] leading-none tracking-none">Team Drivers</span>
 
+        <div className='profiles w-full flex flex-col lg:flex-row gap-62.5 mx-auto mt-25'>
+          <div className='driver1'>
+            <div className='flex flex-col gap-[11px]'>
+              <p className='font-archivo font-bold text-[20px] lg:text-[40px] leading-none tracking-none'>George Russell</p>
+              <p>Main Driver<br />No.63</p>
+              <img className='w-[97px] lg:w-[158px] h-auto' src={arrow} alt="Arrow" />
+            </div>
+          </div>
+
+          <div className='driver2'></div>
+        </div>
       </section>
     </div>
   );
