c676700e5840c186a0f0cde8d3b3b5ffe74492f8
소은 <soeunl131@gmail.com>
2026-04-14 19:03:57 +0900

    team 섹션 수정

--- src/pages/Home/sections/TeamOverview/TeamOverview.jsx
+++ src/pages/Home/sections/TeamOverview/TeamOverview.jsx
@@ -22,14 +22,14 @@ const TeamOverview = forwardRef(({ onScrollUp, isActive }, ref) => {
   }, [isActive, onScrollUp]);
 
   return (
-    <section className="bg-[#000] pt-45 pb-50 w-full"
+    <section className="bg-[#000] pt-45 pb-50 w-full flex flex-col items-center"
       ref={ref}
     >
-      <section className='overview'>
+      <section className='overview w-full lg:w-[1360px] px-12 lg:px-0'>
         <span className="text-[#00F4D0]/70 font-pretendard font-semibold text-[26px] leading-none tracking-none">Team Overview</span>
 
-        <div className='container'>
-          <div className='contents flex flex-col lg:flex-row justify-between lg:max-w-[1360px] w-full mx-auto mt-25 px-12 lg:px-0'>
+        <div className='container w-full flex flex-col gap-[123px] justify-center items-center'>
+          <div className='contents w-full flex flex-col lg:flex-row justify-between mx-auto mt-25'>
             <div className='desc'>
               <h1 className="w-[636px] h-auto mb-10
                 text-[#fff] font-archivo font-bold italic text-[80px] leading-none tracking-none"
@@ -39,7 +39,7 @@ const TeamOverview = forwardRef(({ onScrollUp, isActive }, ref) => {
                 Legacy.
               </h1>
 
-              <p className='text-white font-pretendard font-light text-[20px] leading-normal tracking-tight'>
+              <p className='text-[#C0C7CE] font-pretendard font-light text-[20px] leading-normal tracking-tight'>
                 독일을 대표하는 메르세데스-AMG 페트로나스 포뮬러 원 팀은<br />
                 수십 년의 레이싱 헤리티지를 바탕으로 최점단 기술력과  정밀한 엔지니어링을 결합해,<br />
                 퍼포먼스의 한계를 끊임없이 확장해 나갑니다.
