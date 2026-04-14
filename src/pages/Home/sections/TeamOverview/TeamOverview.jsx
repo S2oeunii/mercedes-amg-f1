5b6bd28051a639897288ab08915ddb0d80011d6b
Soeun Ryu <soeunl131@gmail.com>
2026-04-14 18:41:18 +0900

    수정중

--- src/pages/Home/sections/TeamOverview/TeamOverview.jsx
+++ src/pages/Home/sections/TeamOverview/TeamOverview.jsx
@@ -26,28 +26,31 @@ const TeamOverview = forwardRef(({ onScrollUp, isActive }, ref) => {
       ref={ref}
     >
       <section className='overview'>
-        <div className='flex flex-col lg:flex-row justify-between lg:max-w-[1360px] w-full mx-auto  px-12 lg:px-0'>
-          <div className='desc'>
-            <span className="text-[#00F4D0]/70 font-pretendard font-semibold text-[26px] leading-none tracking-none">Team Overview</span>
-            <h1 className="w-[636px] h-auto mt-25 mb-10
-              text-[#fff] font-archivo font-bold italic text-[80px] leading-none tracking-none"
-            >
-              Precison.<br />
-              <p className='text-right'>Performance.</p>
-              Legacy.
-            </h1>
+        <span className="text-[#00F4D0]/70 font-pretendard font-semibold text-[26px] leading-none tracking-none">Team Overview</span>
 
-            <p className='text-white font-pretendard font-light text-[20px] leading-normal tracking-tight'>
-              독일을 대표하는 메르세데스-AMG 페트로나스 포뮬러 원 팀은<br />
-              수십 년의 레이싱 헤리티지를 바탕으로 최점단 기술력과  정밀한 엔지니어링을 결합해,<br />
-              퍼포먼스의 한계를 끊임없이 확장해 나갑니다.
-            </p>
+        <div className='container'>
+          <div className='contents flex flex-col lg:flex-row justify-between lg:max-w-[1360px] w-full mx-auto mt-25 px-12 lg:px-0'>
+            <div className='desc'>
+              <h1 className="w-[636px] h-auto mb-10
+                text-[#fff] font-archivo font-bold italic text-[80px] leading-none tracking-none"
+              >
+                Precison.<br />
+                <p className='text-right'>Performance.</p>
+                Legacy.
+              </h1>
+
+              <p className='text-white font-pretendard font-light text-[20px] leading-normal tracking-tight'>
+                독일을 대표하는 메르세데스-AMG 페트로나스 포뮬러 원 팀은<br />
+                수십 년의 레이싱 헤리티지를 바탕으로 최점단 기술력과  정밀한 엔지니어링을 결합해,<br />
+                퍼포먼스의 한계를 끊임없이 확장해 나갑니다.
+              </p>
+            </div>
+
+            <div className='w-40 h-80 bg-red-500'></div>
           </div>
 
-          <div className='w-40 h-80 bg-red-500'></div>
+          <MoreView />
         </div>
-
-        <MoreView />
       </section>
     </section>
   );
